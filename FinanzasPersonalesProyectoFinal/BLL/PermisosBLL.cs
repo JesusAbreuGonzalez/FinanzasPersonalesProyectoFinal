ff11ec6999375a32390fe38025b62046462c3dad
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-17 23:30:48 -0400

    Agregnado PresupuestosBLL y PermisosBLL

---
+++ FinanzasPersonalesProyectoFinal/BLL/PermisosBLL.cs
@@ -0,0 +1,155 @@
+﻿using FinanzasPersonalesProyectoFinal.DAL;
+using FinanzasPersonalesProyectoFinal.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzasPersonalesProyectoFinal.BLL
+{
+    public class PermisosBLL
+    {
+         public static bool Existe(string descripcion)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Permisos.Any(e => e.Descripcion.ToLower() == descripcion.ToLower());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        private static bool Insertar(Permisos permisos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Permisos.Add(permisos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Modificar(Permisos permisos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(permisos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Permisos permisos)
+        {
+            if (!Existe(permisos.Descripcion))
+                return Insertar(permisos);
+            else
+                return Modificar(permisos);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var permisos = contexto.Permisos.Find(id);
+                if (permisos != null)
+                {
+                    contexto.Permisos.Remove(permisos);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Permisos Buscar(int id)
+        {
+            var contexto = new Contexto();
+            Permisos permisos;
+
+            try
+            {
+                permisos = contexto.Permisos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return permisos;
+        }
+
+        public static List<Permisos> GetList(Expression<Func<Permisos, bool>> criterio)
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Permisos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
---
+++ FinanzasPersonalesProyectoFinal/BLL/PresupuestosBLL.cs
@@ -0,0 +1,155 @@
+﻿using FinanzasPersonalesProyectoFinal.DAL;
+using FinanzasPersonalesProyectoFinal.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzasPersonalesProyectoFinal.BLL
+{
+    public class PresupuestosBLL
+    {
+        public static bool Existe(string nombre)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Presupuestos.Any(e => e.NombrePresupuesto.ToLower() == nombre.ToLower());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        private static bool Insertar(Presupuestos presupuestos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Presupuestos.Add(presupuestos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Modificar(Presupuestos presupuestos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(presupuestos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Presupuestos presupuestos)
+        {
+            if (!Existe(presupuestos.NombrePresupuesto))
+                return Insertar(presupuestos);
+            else
+                return Modificar(presupuestos);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var presupuestos = contexto.Presupuestos.Find(id);
+                if (presupuestos != null)
+                {
+                    contexto.Presupuestos.Remove(presupuestos);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Presupuestos Buscar(int id)
+        {
+            var contexto = new Contexto();
+            Presupuestos presupuestos;
+
+            try
+            {
+                presupuestos = contexto.Presupuestos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return presupuestos;
+        }
+
+        public static List<Presupuestos> GetList(Expression<Func<Presupuestos, bool>> criterio)
+        {
+            List<Presupuestos> lista = new List<Presupuestos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Presupuestos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
