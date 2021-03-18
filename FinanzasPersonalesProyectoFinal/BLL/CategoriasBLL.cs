f101fde4a14a21e4b12748b1ad79ef852a7601c9
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-17 22:47:07 -0400

    Agregando CategoriasBLL y GastosBLL

---
+++ FinanzasPersonalesProyectoFinal/BLL/CategoriasBLL.cs
@@ -0,0 +1,156 @@
+﻿using FinanzasPersonalesProyectoFinal.DAL;
+using FinanzasPersonalesProyectoFinal.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzasPersonalesProyectoFinal.BLL
+{
+    public class CategoriasBLL
+    { 
+        public static bool ExisteCategoria(string nombre)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Categorias.Any(e => e.NombreCategoria.ToLower() == nombre.ToLower());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        private static bool Insertar(Categorias categorias)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                contexto.Categorias.Add(categorias);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Modificar(Categorias categorias)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(categorias).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Categorias categorias)
+        {
+            if (!ExisteCategoria(categorias.NombreCategoria))
+                return Insertar(categorias);
+            else
+                return Modificar(categorias);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                var categorias = contexto.Categorias.Find(id);
+                if(categorias != null)
+                {
+                    contexto.Categorias.Remove(categorias);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Categorias Buscar(int id)
+        {
+            var contexto = new Contexto();
+            Categorias categorias;
+
+            try
+            {
+                categorias = contexto.Categorias.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return categorias;
+        }
+
+        public static List<Categorias> GetList(Expression<Func<Categorias, bool>> criterio)
+        {
+            List<Categorias> lista = new List<Categorias>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Categorias.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
---
+++ FinanzasPersonalesProyectoFinal/BLL/GastosBLL.cs
@@ -0,0 +1,182 @@
+﻿using FinanzasPersonalesProyectoFinal.DAL;
+using FinanzasPersonalesProyectoFinal.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzasPersonalesProyectoFinal.BLL
+{
+    public class GastosBLL
+    {
+        public static bool ExisteGasto(string descripcion)
+        {
+            bool encontrado = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Gastos.Any(e => e.DescripcionGasto.ToLower() == descripcion.ToLower());
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        private static bool Insertar(Gastos gastos)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                contexto.Gastos.Add(gastos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        //Metodo para modificar los gastos
+        private static bool Modificar(Gastos gastos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"DELETE FROM GastosDetalle Where GastoId = {gastos.GastoId}");
+                foreach (var anterior in gastos.GastosDetalle)
+                {
+                    contexto.Entry(anterior).State = EntityState.Added;
+                }
+                contexto.Entry(gastos).State = EntityState.Modified;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+
+        }
+
+        public static bool Guardar(Gastos gastos)
+        {
+            if (!ExisteGasto(gastos.DescripcionGasto))
+                return Insertar(gastos);
+            else
+                return Modificar(gastos);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var eliminar = contexto.Gastos.Find(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Gastos Buscar(int id)
+        {
+            var contexto = new Contexto();
+            var gastos = new Gastos();
+
+            try
+            {
+                gastos = contexto.Gastos.Include(x => x.GastosDetalle).Where(p => p.GastoId == id).SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return gastos;
+        }
+
+        public static List<Gastos> GetList(Expression<Func<Gastos, bool>> criterio)
+        {
+            List<Gastos> lista = new List<Gastos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                //Obtenemos la lista y la filtramos segun el criterio recibido por parametro
+                lista = contexto.Gastos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<Gastos> GetRoles()
+        {
+            List<Gastos> lista = new List<Gastos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Gastos.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
