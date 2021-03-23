0393b25ac339aaac509b6c06a4bd2f2f895a448c
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-23 19:32:49 -0400

    Agregando clase Utilidades.
    Creando cUsuarios.

---
+++ FinanzasPersonalesProyectoFinal/UI/Consultas/cUsuarios.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace FinanzasPersonalesProyectoFinal.UI.Consultas
+{
+    partial class cUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsuariosConsultaDataGridView = new System.Windows.Forms.DataGridView();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.ActivoGroupBox = new System.Windows.Forms.GroupBox();
+            this.InactivosRadioButton = new System.Windows.Forms.RadioButton();
+            this.ActivosRadioButton = new System.Windows.Forms.RadioButton();
+            this.TodosRadioButton = new System.Windows.Forms.RadioButton();
+            this.CriterioTextBox = new System.Windows.Forms.TextBox();
+            this.GroupBox = new System.Windows.Forms.GroupBox();
+            this.HastaDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DesdeDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FiltroComboBox = new System.Windows.Forms.ComboBox();
+            this.FechaCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosConsultaDataGridView)).BeginInit();
+            this.ActivoGroupBox.SuspendLayout();
+            this.GroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UsuariosConsultaDataGridView
+            // 
+            this.UsuariosConsultaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsuariosConsultaDataGridView.Location = new System.Drawing.Point(12, 117);
+            this.UsuariosConsultaDataGridView.Name = "UsuariosConsultaDataGridView";
+            this.UsuariosConsultaDataGridView.RowHeadersWidth = 51;
+            this.UsuariosConsultaDataGridView.RowTemplate.Height = 29;
+            this.UsuariosConsultaDataGridView.Size = new System.Drawing.Size(964, 435);
+            this.UsuariosConsultaDataGridView.TabIndex = 0;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Image = global::FinanzasPersonalesProyectoFinal.Properties.Resources.lupa;
+            this.BuscarButton.Location = new System.Drawing.Point(862, 22);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(115, 86);
+            this.BuscarButton.TabIndex = 1;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            this.BuscarButton.Click += new System.EventHandler(this.BuscarButton_Click);
+            // 
+            // ActivoGroupBox
+            // 
+            this.ActivoGroupBox.Controls.Add(this.InactivosRadioButton);
+            this.ActivoGroupBox.Controls.Add(this.ActivosRadioButton);
+            this.ActivoGroupBox.Controls.Add(this.TodosRadioButton);
+            this.ActivoGroupBox.Location = new System.Drawing.Point(733, 9);
+            this.ActivoGroupBox.Name = "ActivoGroupBox";
+            this.ActivoGroupBox.Size = new System.Drawing.Size(123, 99);
+            this.ActivoGroupBox.TabIndex = 2;
+            this.ActivoGroupBox.TabStop = false;
+            this.ActivoGroupBox.Text = "EsActivo?";
+            // 
+            // InactivosRadioButton
+            // 
+            this.InactivosRadioButton.AutoSize = true;
+            this.InactivosRadioButton.Location = new System.Drawing.Point(6, 68);
+            this.InactivosRadioButton.Name = "InactivosRadioButton";
+            this.InactivosRadioButton.Size = new System.Drawing.Size(82, 24);
+            this.InactivosRadioButton.TabIndex = 2;
+            this.InactivosRadioButton.Text = "Inactivo";
+            this.InactivosRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ActivosRadioButton
+            // 
+            this.ActivosRadioButton.AutoSize = true;
+            this.ActivosRadioButton.Location = new System.Drawing.Point(6, 44);
+            this.ActivosRadioButton.Name = "ActivosRadioButton";
+            this.ActivosRadioButton.Size = new System.Drawing.Size(78, 24);
+            this.ActivosRadioButton.TabIndex = 1;
+            this.ActivosRadioButton.Text = "Activos";
+            this.ActivosRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // TodosRadioButton
+            // 
+            this.TodosRadioButton.AutoSize = true;
+            this.TodosRadioButton.Checked = true;
+            this.TodosRadioButton.Location = new System.Drawing.Point(6, 20);
+            this.TodosRadioButton.Name = "TodosRadioButton";
+            this.TodosRadioButton.Size = new System.Drawing.Size(70, 24);
+            this.TodosRadioButton.TabIndex = 0;
+            this.TodosRadioButton.TabStop = true;
+            this.TodosRadioButton.Text = "Todos";
+            this.TodosRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CriterioTextBox
+            // 
+            this.CriterioTextBox.Location = new System.Drawing.Point(206, 74);
+            this.CriterioTextBox.Name = "CriterioTextBox";
+            this.CriterioTextBox.Size = new System.Drawing.Size(521, 27);
+            this.CriterioTextBox.TabIndex = 3;
+            // 
+            // GroupBox
+            // 
+            this.GroupBox.Controls.Add(this.HastaDateTimePicker);
+            this.GroupBox.Controls.Add(this.label2);
+            this.GroupBox.Controls.Add(this.DesdeDateTimePicker);
+            this.GroupBox.Controls.Add(this.label1);
+            this.GroupBox.Location = new System.Drawing.Point(206, 9);
+            this.GroupBox.Name = "GroupBox";
+            this.GroupBox.Size = new System.Drawing.Size(521, 59);
+            this.GroupBox.TabIndex = 4;
+            this.GroupBox.TabStop = false;
+            // 
+            // HastaDateTimePicker
+            // 
+            this.HastaDateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.HastaDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.HastaDateTimePicker.Location = new System.Drawing.Point(359, 20);
+            this.HastaDateTimePicker.Name = "HastaDateTimePicker";
+            this.HastaDateTimePicker.Size = new System.Drawing.Size(156, 27);
+            this.HastaDateTimePicker.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hasta";
+            // 
+            // DesdeDateTimePicker
+            // 
+            this.DesdeDateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.DesdeDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DesdeDateTimePicker.Location = new System.Drawing.Point(63, 20);
+            this.DesdeDateTimePicker.Name = "DesdeDateTimePicker";
+            this.DesdeDateTimePicker.Size = new System.Drawing.Size(156, 27);
+            this.DesdeDateTimePicker.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Filtro";
+            // 
+            // FiltroComboBox
+            // 
+            this.FiltroComboBox.FormattingEnabled = true;
+            this.FiltroComboBox.Location = new System.Drawing.Point(61, 74);
+            this.FiltroComboBox.Name = "FiltroComboBox";
+            this.FiltroComboBox.Size = new System.Drawing.Size(121, 28);
+            this.FiltroComboBox.TabIndex = 5;
+            // 
+            // FechaCheckBox
+            // 
+            this.FechaCheckBox.AutoSize = true;
+            this.FechaCheckBox.Location = new System.Drawing.Point(12, 33);
+            this.FechaCheckBox.Name = "FechaCheckBox";
+            this.FechaCheckBox.Size = new System.Drawing.Size(163, 24);
+            this.FechaCheckBox.TabIndex = 6;
+            this.FechaCheckBox.Text = "Usar filtro de fechas";
+            this.FechaCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 567);
+            this.Controls.Add(this.FechaCheckBox);
+            this.Controls.Add(this.FiltroComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.GroupBox);
+            this.Controls.Add(this.CriterioTextBox);
+            this.Controls.Add(this.ActivoGroupBox);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.UsuariosConsultaDataGridView);
+            this.Name = "cUsuarios";
+            this.Text = "Consulta Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosConsultaDataGridView)).EndInit();
+            this.ActivoGroupBox.ResumeLayout(false);
+            this.ActivoGroupBox.PerformLayout();
+            this.GroupBox.ResumeLayout(false);
+            this.GroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView UsuariosConsultaDataGridView;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.GroupBox ActivoGroupBox;
+        private System.Windows.Forms.RadioButton InactivosRadioButton;
+        private System.Windows.Forms.RadioButton ActivosRadioButton;
+        private System.Windows.Forms.RadioButton TodosRadioButton;
+        private System.Windows.Forms.TextBox CriterioTextBox;
+        private System.Windows.Forms.GroupBox GroupBox;
+        private System.Windows.Forms.DateTimePicker HastaDateTimePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DesdeDateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox FiltroComboBox;
+        private System.Windows.Forms.CheckBox FechaCheckBox;
+    }
+}
---
+++ FinanzasPersonalesProyectoFinal/UI/Consultas/cUsuarios.cs
@@ -0,0 +1,223 @@
+﻿using FinanzasPersonalesProyectoFinal.BLL;
+using FinanzasPersonalesProyectoFinal.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinanzasPersonalesProyectoFinal.UI.Consultas
+{
+    public partial class cUsuarios : Form
+    {
+        List<Usuarios> lista = new List<Usuarios>();
+        public cUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, EventArgs e)
+        {
+            //En caso de que TodosRadioButton se encuentre seleccionado
+            if (TodosRadioButton.Checked)
+            {
+                //Si el filtro de la fecha se encuentra marcado, va a tomar en cuenta le rango de fecha
+                if (FechaCheckBox.Checked)
+                {
+                    if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                    {
+                        switch (FiltroComboBox.SelectedIndex)
+                        {
+                            case 0: //UsuarioId
+                                lista = UsuariosBLL.GetList(r => r.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value));
+                                break;
+                            case 1: //Alias
+                                lista = UsuariosBLL.GetList(r => r.Alias.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value));
+                                break;
+                            case 2: //Nombres
+                                lista = UsuariosBLL.GetList(r => r.Nombres.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value));
+                                break;
+                            case 3: //Email
+                                lista = UsuariosBLL.GetList(r => r.Email.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value));
+                                break;
+                            case 4: //RolId
+                                lista = UsuariosBLL.GetList(r => r.RolId == Utilidades.ToInt(CriterioTextBox.Text) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //En caso de que no haya nada en el textBox, se imprimirán todos los usuarios
+                    else
+                        lista = UsuariosBLL.GetList(r => true);
+                }
+                else
+                {
+                    if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                    {
+                        switch (FiltroComboBox.SelectedIndex)
+                        {
+                            case 0: //UsuarioId
+                                lista = UsuariosBLL.GetList(r => r.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text));
+                                break;
+                            case 1: //Alias
+                                lista = UsuariosBLL.GetList(r => r.Alias.Contains(CriterioTextBox.Text.ToUpper()));
+                                break;
+                            case 2: //Nombres
+                                lista = UsuariosBLL.GetList(r => r.Nombres.Contains(CriterioTextBox.Text.ToUpper()));
+                                break;
+                            case 3: //Email
+                                lista = UsuariosBLL.GetList(r => r.Email.Contains(CriterioTextBox.Text.ToUpper()));
+                                break;
+                            case 4: //RolId
+                                lista = UsuariosBLL.GetList(r => r.RolId == Utilidades.ToInt(CriterioTextBox.Text));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //En caso de que no haya nada en el textBox, se imprimirán todos los usuarios
+                    else
+                        lista = UsuariosBLL.GetList(r => true);
+                }
+                
+            }
+            //En caso de que ActivosRadioButton se encuentre seleccionado
+            else if (ActivosRadioButton.Checked)
+            {
+                //Si el filtro de la fecha se encuentra marcado, va a tomar en cuenta le rango de fecha
+                if (FechaCheckBox.Checked)
+                {
+                    if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                    {
+                        switch (FiltroComboBox.SelectedIndex)
+                        {
+                            case 0: //UsuarioId
+                                lista = UsuariosBLL.GetList(r => r.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && r.Activo);
+                                break;
+                            case 1: //Alias
+                                lista = UsuariosBLL.GetList(r => r.Alias.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && r.Activo);
+                                break;
+                            case 2: //Nombres
+                                lista = UsuariosBLL.GetList(r => r.Nombres.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && r.Activo);
+                                break;
+                            case 3: //Email
+                                lista = UsuariosBLL.GetList(r => r.Email.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && r.Activo);
+                                break;
+                            case 4: //RolId
+                                lista = UsuariosBLL.GetList(r => r.RolId == Utilidades.ToInt(CriterioTextBox.Text) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && r.Activo);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //En caso de que no haya nada en el textBox, se imprimirán todos los usuarios
+                    else
+                        lista = UsuariosBLL.GetList(r => true);
+                }
+                else
+                {
+                    if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                    {
+                        switch (FiltroComboBox.SelectedIndex)
+                        {
+                            case 0: //UsuarioId
+                                lista = UsuariosBLL.GetList(r => r.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text) && r.Activo);
+                                break;
+                            case 1: //Alias
+                                lista = UsuariosBLL.GetList(r => r.Alias.Contains(CriterioTextBox.Text.ToUpper()) && r.Activo);
+                                break;
+                            case 2: //Nombres
+                                lista = UsuariosBLL.GetList(r => r.Nombres.Contains(CriterioTextBox.Text.ToUpper()) && r.Activo);
+                                break;
+                            case 3: //Email
+                                lista = UsuariosBLL.GetList(r => r.Email.Contains(CriterioTextBox.Text.ToUpper()) && r.Activo);
+                                break;
+                            case 4: //RolId
+                                lista = UsuariosBLL.GetList(r => r.RolId == Utilidades.ToInt(CriterioTextBox.Text) && r.Activo);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //En caso de que no haya nada en el textBox, se imprimirán todos los usuarios
+                    else
+                        lista = UsuariosBLL.GetList(r => true);
+                }
+
+            }
+            //En caso de que InactivosRadioButton se encuentre seleccionado
+            else if (InactivosRadioButton.Checked)
+            {
+                //Si el filtro de la fecha se encuentra marcado, va a tomar en cuenta le rango de fecha
+                if (FechaCheckBox.Checked)
+                {
+                    if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                    {
+                        switch (FiltroComboBox.SelectedIndex)
+                        {
+                            case 0: //UsuarioId
+                                lista = UsuariosBLL.GetList(r => r.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && !r.Activo);
+                                break;
+                            case 1: //Alias
+                                lista = UsuariosBLL.GetList(r => r.Alias.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && !r.Activo);
+                                break;
+                            case 2: //Nombres
+                                lista = UsuariosBLL.GetList(r => r.Nombres.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && !r.Activo);
+                                break;
+                            case 3: //Email
+                                lista = UsuariosBLL.GetList(r => r.Email.Contains(CriterioTextBox.Text.ToUpper()) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && !r.Activo);
+                                break;
+                            case 4: //RolId
+                                lista = UsuariosBLL.GetList(r => r.RolId == Utilidades.ToInt(CriterioTextBox.Text) && (r.FechaIngreso >= DesdeDateTimePicker.Value || r.FechaIngreso <= HastaDateTimePicker.Value) && !r.Activo);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //En caso de que no haya nada en el textBox, se imprimirán todos los usuarios
+                    else
+                        lista = UsuariosBLL.GetList(r => true);
+                }
+                else
+                {
+                    if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                    {
+                        switch (FiltroComboBox.SelectedIndex)
+                        {
+                            case 0: //UsuarioId
+                                lista = UsuariosBLL.GetList(r => r.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text) && !r.Activo);
+                                break;
+                            case 1: //Alias
+                                lista = UsuariosBLL.GetList(r => r.Alias.Contains(CriterioTextBox.Text.ToUpper()) && !r.Activo);
+                                break;
+                            case 2: //Nombres
+                                lista = UsuariosBLL.GetList(r => r.Nombres.Contains(CriterioTextBox.Text.ToUpper()) && !r.Activo);
+                                break;
+                            case 3: //Email
+                                lista = UsuariosBLL.GetList(r => r.Email.Contains(CriterioTextBox.Text.ToUpper()) && !r.Activo);
+                                break;
+                            case 4: //RolId
+                                lista = UsuariosBLL.GetList(r => r.RolId == Utilidades.ToInt(CriterioTextBox.Text) && !r.Activo);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //En caso de que no haya nada en el textBox, se imprimirán todos los usuarios
+                    else
+                        lista = UsuariosBLL.GetList(r => true);
+                }
+
+            }
+
+
+            UsuariosConsultaDataGridView.DataSource = null;
+            UsuariosConsultaDataGridView.DataSource = lista;
+        }
+    }
+}
