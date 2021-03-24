0207c41a15166467cf3e8af0b224f4f4b0d3eec1
JoseDanielVeras <josedaniel510jd@gmail.com>
2021-03-24 10:37:56 -0400

    Agregando eventos del registro de ingresos.

--- FinanzasPersonalesProyectoFinal/UI/Registros/rIngresos.Designer.cs
+++ FinanzasPersonalesProyectoFinal/UI/Registros/rIngresos.Designer.cs
@@ -29,6 +29,7 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rIngresos));
             this.EliminarButton = new System.Windows.Forms.Button();
             this.GuardarButton = new System.Windows.Forms.Button();
@@ -41,14 +42,16 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             this.RemoverButton = new System.Windows.Forms.Button();
             this.AgregarButton = new System.Windows.Forms.Button();
             this.IngresosDataGridView = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MontoTextBox = new System.Windows.Forms.TextBox();
             this.MontoLabel = new System.Windows.Forms.Label();
             this.PresupuestoLabel = new System.Windows.Forms.Label();
             this.DescripcionLabel = new System.Windows.Forms.Label();
             this.IdGastosLabel = new System.Windows.Forms.Label();
+            this.IngresosErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.IngresosIdNumericUpDown)).BeginInit();
             this.DetalleGastosGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.IngresosDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IngresosErrorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // EliminarButton
@@ -56,24 +59,23 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             this.EliminarButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.EliminarButton.Image = ((System.Drawing.Image)(resources.GetObject("EliminarButton.Image")));
             this.EliminarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.EliminarButton.Location = new System.Drawing.Point(376, 571);
-            this.EliminarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.EliminarButton.Location = new System.Drawing.Point(329, 428);
             this.EliminarButton.Name = "EliminarButton";
-            this.EliminarButton.Size = new System.Drawing.Size(103, 53);
+            this.EliminarButton.Size = new System.Drawing.Size(90, 40);
             this.EliminarButton.TabIndex = 33;
             this.EliminarButton.Text = "Eliminar";
             this.EliminarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.EliminarButton.UseVisualStyleBackColor = true;
+            this.EliminarButton.Click += new System.EventHandler(this.EliminarButton_Click);
             // 
             // GuardarButton
             // 
             this.GuardarButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.GuardarButton.Image = ((System.Drawing.Image)(resources.GetObject("GuardarButton.Image")));
             this.GuardarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.GuardarButton.Location = new System.Drawing.Point(253, 571);
-            this.GuardarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.GuardarButton.Location = new System.Drawing.Point(221, 428);
             this.GuardarButton.Name = "GuardarButton";
-            this.GuardarButton.Size = new System.Drawing.Size(103, 53);
+            this.GuardarButton.Size = new System.Drawing.Size(90, 40);
             this.GuardarButton.TabIndex = 32;
             this.GuardarButton.Text = "Guardar";
             this.GuardarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -85,48 +87,46 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             this.NuevoButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.NuevoButton.Image = ((System.Drawing.Image)(resources.GetObject("NuevoButton.Image")));
             this.NuevoButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.NuevoButton.Location = new System.Drawing.Point(129, 571);
-            this.NuevoButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NuevoButton.Location = new System.Drawing.Point(113, 428);
             this.NuevoButton.Name = "NuevoButton";
-            this.NuevoButton.Size = new System.Drawing.Size(103, 53);
+            this.NuevoButton.Size = new System.Drawing.Size(90, 40);
             this.NuevoButton.TabIndex = 31;
             this.NuevoButton.Text = "Nuevo";
             this.NuevoButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.NuevoButton.UseVisualStyleBackColor = true;
+            this.NuevoButton.Click += new System.EventHandler(this.NuevoButton_Click);
             // 
             // BuscarButton
             // 
             this.BuscarButton.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButton.Image")));
-            this.BuscarButton.Location = new System.Drawing.Point(245, 48);
-            this.BuscarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BuscarButton.Location = new System.Drawing.Point(214, 36);
             this.BuscarButton.Name = "BuscarButton";
-            this.BuscarButton.Size = new System.Drawing.Size(70, 35);
+            this.BuscarButton.Size = new System.Drawing.Size(61, 26);
             this.BuscarButton.TabIndex = 30;
             this.BuscarButton.UseVisualStyleBackColor = true;
+            this.BuscarButton.Click += new System.EventHandler(this.BuscarButton_Click);
             // 
             // PresupuestoIdComboBox
             // 
+            this.PresupuestoIdComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.PresupuestoIdComboBox.FormattingEnabled = true;
-            this.PresupuestoIdComboBox.Location = new System.Drawing.Point(329, 120);
-            this.PresupuestoIdComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.PresupuestoIdComboBox.Location = new System.Drawing.Point(288, 90);
             this.PresupuestoIdComboBox.Name = "PresupuestoIdComboBox";
-            this.PresupuestoIdComboBox.Size = new System.Drawing.Size(263, 28);
+            this.PresupuestoIdComboBox.Size = new System.Drawing.Size(231, 23);
             this.PresupuestoIdComboBox.TabIndex = 29;
             // 
             // DescipcionTextBox
             // 
-            this.DescipcionTextBox.Location = new System.Drawing.Point(16, 120);
-            this.DescipcionTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DescipcionTextBox.Location = new System.Drawing.Point(14, 90);
             this.DescipcionTextBox.Name = "DescipcionTextBox";
-            this.DescipcionTextBox.Size = new System.Drawing.Size(298, 27);
+            this.DescipcionTextBox.Size = new System.Drawing.Size(261, 23);
             this.DescipcionTextBox.TabIndex = 28;
             // 
             // IngresosIdNumericUpDown
             // 
-            this.IngresosIdNumericUpDown.Location = new System.Drawing.Point(18, 48);
-            this.IngresosIdNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.IngresosIdNumericUpDown.Location = new System.Drawing.Point(16, 36);
             this.IngresosIdNumericUpDown.Name = "IngresosIdNumericUpDown";
-            this.IngresosIdNumericUpDown.Size = new System.Drawing.Size(219, 27);
+            this.IngresosIdNumericUpDown.Size = new System.Drawing.Size(192, 23);
             this.IngresosIdNumericUpDown.TabIndex = 27;
             // 
             // DetalleGastosGroupBox
@@ -134,13 +134,11 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             this.DetalleGastosGroupBox.Controls.Add(this.RemoverButton);
             this.DetalleGastosGroupBox.Controls.Add(this.AgregarButton);
             this.DetalleGastosGroupBox.Controls.Add(this.IngresosDataGridView);
-            this.DetalleGastosGroupBox.Controls.Add(this.textBox1);
+            this.DetalleGastosGroupBox.Controls.Add(this.MontoTextBox);
             this.DetalleGastosGroupBox.Controls.Add(this.MontoLabel);
-            this.DetalleGastosGroupBox.Location = new System.Drawing.Point(9, 161);
-            this.DetalleGastosGroupBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DetalleGastosGroupBox.Location = new System.Drawing.Point(8, 121);
             this.DetalleGastosGroupBox.Name = "DetalleGastosGroupBox";
-            this.DetalleGastosGroupBox.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.DetalleGastosGroupBox.Size = new System.Drawing.Size(591, 401);
+            this.DetalleGastosGroupBox.Size = new System.Drawing.Size(517, 301);
             this.DetalleGastosGroupBox.TabIndex = 26;
             this.DetalleGastosGroupBox.TabStop = false;
             this.DetalleGastosGroupBox.Text = "Detalle del Ingreso";
@@ -150,54 +148,52 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             this.RemoverButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.RemoverButton.Image = ((System.Drawing.Image)(resources.GetObject("RemoverButton.Image")));
             this.RemoverButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.RemoverButton.Location = new System.Drawing.Point(466, 55);
-            this.RemoverButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.RemoverButton.Location = new System.Drawing.Point(408, 41);
             this.RemoverButton.Name = "RemoverButton";
-            this.RemoverButton.Size = new System.Drawing.Size(118, 35);
+            this.RemoverButton.Size = new System.Drawing.Size(103, 26);
             this.RemoverButton.TabIndex = 8;
             this.RemoverButton.Text = "Remover";
             this.RemoverButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.RemoverButton.UseVisualStyleBackColor = true;
+            this.RemoverButton.Click += new System.EventHandler(this.RemoverButton_Click);
             // 
             // AgregarButton
             // 
             this.AgregarButton.Image = ((System.Drawing.Image)(resources.GetObject("AgregarButton.Image")));
             this.AgregarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.AgregarButton.Location = new System.Drawing.Point(320, 55);
-            this.AgregarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AgregarButton.Location = new System.Drawing.Point(280, 41);
             this.AgregarButton.Name = "AgregarButton";
-            this.AgregarButton.Size = new System.Drawing.Size(118, 35);
+            this.AgregarButton.Size = new System.Drawing.Size(103, 26);
             this.AgregarButton.TabIndex = 7;
             this.AgregarButton.Text = "Agregar";
             this.AgregarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.AgregarButton.UseVisualStyleBackColor = true;
+            this.AgregarButton.Click += new System.EventHandler(this.AgregarButton_Click);
             // 
             // IngresosDataGridView
             // 
             this.IngresosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.IngresosDataGridView.Location = new System.Drawing.Point(7, 100);
-            this.IngresosDataGridView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.IngresosDataGridView.Location = new System.Drawing.Point(6, 75);
             this.IngresosDataGridView.Name = "IngresosDataGridView";
             this.IngresosDataGridView.RowHeadersWidth = 51;
             this.IngresosDataGridView.RowTemplate.Height = 25;
-            this.IngresosDataGridView.Size = new System.Drawing.Size(577, 293);
+            this.IngresosDataGridView.Size = new System.Drawing.Size(505, 220);
             this.IngresosDataGridView.TabIndex = 6;
             // 
-            // textBox1
+            // MontoTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 55);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(298, 27);
-            this.textBox1.TabIndex = 5;
+            this.MontoTextBox.Location = new System.Drawing.Point(6, 41);
+            this.MontoTextBox.Name = "MontoTextBox";
+            this.MontoTextBox.Size = new System.Drawing.Size(261, 23);
+            this.MontoTextBox.TabIndex = 5;
             // 
             // MontoLabel
             // 
             this.MontoLabel.AutoSize = true;
             this.MontoLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.MontoLabel.Location = new System.Drawing.Point(7, 28);
+            this.MontoLabel.Location = new System.Drawing.Point(6, 21);
             this.MontoLabel.Name = "MontoLabel";
-            this.MontoLabel.Size = new System.Drawing.Size(61, 23);
+            this.MontoLabel.Size = new System.Drawing.Size(47, 17);
             this.MontoLabel.TabIndex = 3;
             this.MontoLabel.Text = "Monto";
             // 
@@ -205,9 +201,9 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             // 
             this.PresupuestoLabel.AutoSize = true;
             this.PresupuestoLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.PresupuestoLabel.Location = new System.Drawing.Point(329, 93);
+            this.PresupuestoLabel.Location = new System.Drawing.Point(288, 70);
             this.PresupuestoLabel.Name = "PresupuestoLabel";
-            this.PresupuestoLabel.Size = new System.Drawing.Size(104, 23);
+            this.PresupuestoLabel.Size = new System.Drawing.Size(80, 17);
             this.PresupuestoLabel.TabIndex = 25;
             this.PresupuestoLabel.Text = "Presupuesto";
             // 
@@ -215,9 +211,9 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             // 
             this.DescripcionLabel.AutoSize = true;
             this.DescripcionLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.DescripcionLabel.Location = new System.Drawing.Point(16, 93);
+            this.DescripcionLabel.Location = new System.Drawing.Point(14, 70);
             this.DescripcionLabel.Name = "DescripcionLabel";
-            this.DescripcionLabel.Size = new System.Drawing.Size(98, 23);
+            this.DescripcionLabel.Size = new System.Drawing.Size(76, 17);
             this.DescripcionLabel.TabIndex = 24;
             this.DescripcionLabel.Text = "Descripción";
             // 
@@ -225,17 +221,21 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             // 
             this.IdGastosLabel.AutoSize = true;
             this.IdGastosLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.IdGastosLabel.Location = new System.Drawing.Point(18, 20);
+            this.IdGastosLabel.Location = new System.Drawing.Point(16, 15);
             this.IdGastosLabel.Name = "IdGastosLabel";
-            this.IdGastosLabel.Size = new System.Drawing.Size(25, 23);
+            this.IdGastosLabel.Size = new System.Drawing.Size(19, 17);
             this.IdGastosLabel.TabIndex = 23;
             this.IdGastosLabel.Text = "Id";
             // 
+            // IngresosErrorProvider
+            // 
+            this.IngresosErrorProvider.ContainerControl = this;
+            // 
             // rIngresos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(609, 644);
+            this.ClientSize = new System.Drawing.Size(533, 483);
             this.Controls.Add(this.EliminarButton);
             this.Controls.Add(this.GuardarButton);
             this.Controls.Add(this.NuevoButton);
@@ -247,13 +247,14 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
             this.Controls.Add(this.PresupuestoLabel);
             this.Controls.Add(this.DescripcionLabel);
             this.Controls.Add(this.IdGastosLabel);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "rIngresos";
             this.Text = "Registro de Ingresos";
+            this.Load += new System.EventHandler(this.rIngresos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.IngresosIdNumericUpDown)).EndInit();
             this.DetalleGastosGroupBox.ResumeLayout(false);
             this.DetalleGastosGroupBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.IngresosDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IngresosErrorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -272,10 +273,11 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
         private System.Windows.Forms.Button RemoverButton;
         private System.Windows.Forms.Button AgregarButton;
         private System.Windows.Forms.DataGridView IngresosDataGridView;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox MontoTextBox;
         private System.Windows.Forms.Label MontoLabel;
         private System.Windows.Forms.Label PresupuestoLabel;
         private System.Windows.Forms.Label DescripcionLabel;
         private System.Windows.Forms.Label IdGastosLabel;
+        private System.Windows.Forms.ErrorProvider IngresosErrorProvider;
     }
 }
--- FinanzasPersonalesProyectoFinal/UI/Registros/rIngresos.cs
+++ FinanzasPersonalesProyectoFinal/UI/Registros/rIngresos.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FinanzasPersonalesProyectoFinal.BLL;
+using FinanzasPersonalesProyectoFinal.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +14,159 @@ namespace FinanzasPersonalesProyectoFinal.UI.Registros
 {
     public partial class rIngresos : Form
     {
+        public List<IngresosDetalle> ingresosDetalle { get; set; }
+        public List<IngresosDetalle> detalle { get; set; }
+
         public rIngresos()
         {
             InitializeComponent();
+            this.ingresosDetalle = new List<IngresosDetalle>();
+        }
+
+        private void LlenarGird()
+        {
+            IngresosDataGridView.DataSource = null;
+            IngresosDataGridView.DataSource = ingresosDetalle;
+        }
+
+        private void LlenarFila()
+        {
+            IngresosDataGridView.DataSource = null;
+            IngresosDataGridView.DataSource = detalle;
+        }
+
+        private void Limpiar()
+        {
+            IngresosIdNumericUpDown.Value = 0;
+            DescipcionTextBox.Clear();
+            MontoTextBox.Clear();
+            IngresosDataGridView.DataSource = null;
+            IngresosErrorProvider.Clear();
+        }
+
+        private void LlenaCampos(Ingresos ingresos)
+        {
+            IngresosIdNumericUpDown.Value = ingresos.IngresoId;
+            DescipcionTextBox.Text = ingresos.DescripcionIngreso;
+            PresupuestoIdComboBox.Text = Convert.ToString(ingresos.PresupuestoId);
+            this.ingresosDetalle = ingresos.IngresosDetalle;
+            LlenarGird();
+        }
+
+        private Ingresos LlenaClases()
+        {
+            Ingresos ingresos = new Ingresos();
+
+            ingresos.IngresoId = (int)IngresosIdNumericUpDown.Value;
+            ingresos.DescripcionIngreso = DescipcionTextBox.Text;
+            ingresos.PresupuestoId = PresupuestoIdComboBox.SelectedIndex;
+            ingresos.IngresosDetalle = this.ingresosDetalle;
+            LlenarGird();
+
+            return ingresos;
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if (DescipcionTextBox.Text == "")
+            {
+                IngresosErrorProvider.SetError(DescipcionTextBox, "Obligatorio");
+                paso = false;
+            }
+
+            if (ingresosDetalle.Count == 0)
+            {
+                IngresosErrorProvider.SetError(IngresosDataGridView, "Agregue algun detalle");
+                paso = false;
+            }
+
+            return paso;
+        }
+
+        private void BuscarButton_Click(object sender, EventArgs e)
+        {
+            var ingresos = new Ingresos();
+            int id = (int)IngresosIdNumericUpDown.Value;
+
+            Limpiar();
+            ingresos = IngresosBLL.Buscar(id);
+
+            if (ingresos != null)
+                LlenaCampos(ingresos);
+            else
+                MessageBox.Show("Ingreso no encontrado", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void AgregarButton_Click(object sender, EventArgs e)
+        {
+            if (IngresosDataGridView.DataSource != null)
+                this.ingresosDetalle = (List<IngresosDetalle>) IngresosDataGridView.DataSource;
+
+            this.ingresosDetalle.Add(
+                new IngresosDetalle()
+                {
+                    DetalleId = (int)IngresosIdNumericUpDown.Value,
+                    MontoIngreso = Convert.ToInt32(MontoTextBox.Text)
+                }
+            );
+
+            detalle = ingresosDetalle;
+            LlenarFila();
+        }
+
+        private void RemoverButton_Click(object sender, EventArgs e)
+        {
+            if(IngresosDataGridView.Rows.Count > 0 && IngresosDataGridView.CurrentRow != null)
+            {
+                ingresosDetalle.RemoveAt(IngresosDataGridView.CurrentRow.Index);
+                LlenarGird();
+            }
+        }
+
+        private void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
         }
 
         private void GuardarButton_Click(object sender, EventArgs e)
         {
+            Ingresos ingresos;
 
+            if (!Validar())
+                return;
+
+            ingresos = LlenaClases();
+            var paso = IngresosBLL.Guardar(ingresos);
+
+            if(paso)
+            {
+                Limpiar();
+                MessageBox.Show("El ingreso ha sido guardado con exito", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("El ingreso no ha sido guardado con exito", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void EliminarButton_Click(object sender, EventArgs e)
+        {
+            int id = (int)IngresosIdNumericUpDown.Value;
+
+            Limpiar();
+
+            if (IngresosBLL.Eliminar(id))
+                MessageBox.Show("El ingreso ha sido eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                IngresosErrorProvider.SetError(IngresosIdNumericUpDown, "Este Id no existe en la base de datos");
+        }
+
+        private void rIngresos_Load(object sender, EventArgs e)
+        {
+            PresupuestoIdComboBox.DataSource = PresupuestosBLL.GetPresupuestos();
+            PresupuestoIdComboBox.DisplayMember = "NombrePresupuesto";
+            PresupuestoIdComboBox.ValueMember = "PresupuestoId";
         }
     }
 }
