708de306e0858e964c33413bfdfaf043e00c70f9
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-27 22:58:47 -0400

    Creando Tests (Falta codificar cada método)

---
+++ FinanzasPersonalesProyectoFinalTests/BLL/CategoriasBLLTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinanzasPersonalesProyectoFinal.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanzasPersonalesProyectoFinal.Entidades;
+
+namespace FinanzasPersonalesProyectoFinal.BLL.Tests
+{
+    [TestClass()]
+    public class CategoriasBLLTests
+    {
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            var categorias = new Categorias();
+            categorias.CategoriaId = 1;
+            categorias.NombreCategoria = "Alimentación";
+            categorias.FechaCreacion = DateTime.Now;
+
+            var paso = CategoriasBLL.Guardar(categorias);
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCategoriasTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ FinanzasPersonalesProyectoFinalTests2/BLL/IngresosBLLTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinanzasPersonalesProyectoFinal.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzasPersonalesProyectoFinal.BLL.Tests
+{
+    [TestClass()]
+    public class IngresosBLLTests
+    {
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetIngresoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
